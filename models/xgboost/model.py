aa2dbdd4d243a338e78c9f185a72f6ff89192b62
Nguyen Quang Tuan <167944168+tuannguyenquang22@users.noreply.github.com>
2025-06-27 06:55:04 +0700

    Update model.py

--- models/xgboost/model.py
+++ models/xgboost/model.py
@@ -1,5 +1,12 @@
 from xgboost import XGBClassifier
 
 
-class Model(XGBClassifier):
+class CustomXGBClassifier(XGBClassifier):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('enable_categorical', True)
+        kwargs.setdefault('tree_method', 'hist')
+        super().__init__(**kwargs)
+        
+        
+class Model(CustomXGBClassifier):
     pass
